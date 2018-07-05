f89f92ade991d472fcaf6639b06c960412ff8de7
phuwn <phuwn29@gmail.com>
2018-07-05 17:05:07 +0700

    udate database

--- database/noi_that.sql
+++ database/noi_that.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 25, 2018 at 02:31 AM
+-- Generation Time: Jul 05, 2018 at 10:03 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -30,11 +30,25 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `ct_don_hang`;
 CREATE TABLE IF NOT EXISTS `ct_don_hang` (
-  `id_don_hang` int(11) NOT NULL AUTO_INCREMENT,
+  `id_don_hang` int(11) NOT NULL,
   `id_san_pham` int(11) NOT NULL,
   `so_luong` int(11) NOT NULL,
-  PRIMARY KEY (`id_don_hang`)
+  PRIMARY KEY (`id_don_hang`,`id_san_pham`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Chi tiết đơn hàng';
+
+--
+-- Dumping data for table `ct_don_hang`
+--
+
+INSERT INTO `ct_don_hang` (`id_don_hang`, `id_san_pham`, `so_luong`) VALUES
+(4, 7, 10),
+(5, 4, 13),
+(5, 7, 10),
+(6, 4, 13),
+(6, 7, 10),
+(7, 3, 1),
+(7, 4, 1),
+(7, 5, 1);
 
 -- --------------------------------------------------------
 
@@ -62,9 +76,23 @@ DROP TABLE IF EXISTS `don_hang`;
 CREATE TABLE IF NOT EXISTS `don_hang` (
   `id_don_hang` int(11) NOT NULL AUTO_INCREMENT,
   `id_khach_hang` int(11) NOT NULL,
+  `ghi_chu` text COLLATE utf8_unicode_ci NOT NULL,
   `trang_thai` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id_don_hang`,`id_khach_hang`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Đơn hàng';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Đơn hàng';
+
+--
+-- Dumping data for table `don_hang`
+--
+
+INSERT INTO `don_hang` (`id_don_hang`, `id_khach_hang`, `ghi_chu`, `trang_thai`) VALUES
+(1, 123, '', 'chưa thanh toán'),
+(2, 4, '', 'chưa thanh toán'),
+(3, 12, '', 'chưa thanh toán'),
+(4, 13, '', 'chưa thanh toán'),
+(5, 14, '', 'chưa thanh toán'),
+(6, 15, '', 'chưa thanh toán'),
+(7, 16, 'lẹ má', 'chưa thanh toán');
 
 -- --------------------------------------------------------
 
@@ -76,11 +104,33 @@ DROP TABLE IF EXISTS `khach_hang`;
 CREATE TABLE IF NOT EXISTS `khach_hang` (
   `id_khach_hang` int(11) NOT NULL AUTO_INCREMENT,
   `ten_khach_hang` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  `sdt_khach_hang` int(11) NOT NULL,
+  `sdt_khach_hang` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `email_khach_hang` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `dc_khach_hang` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id_khach_hang`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Khách hàng';
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Khách hàng';
+
+--
+-- Dumping data for table `khach_hang`
+--
+
+INSERT INTO `khach_hang` (`id_khach_hang`, `ten_khach_hang`, `sdt_khach_hang`, `email_khach_hang`, `dc_khach_hang`) VALUES
+(1, 'Nghĩa', '123456789', 'haha@nghia.com', 'safsdfsadfasd'),
+(2, 'NghÄ©a', '123456789', 'haha@nghia.com', 'safsdfsadfasd'),
+(3, '', '123456789', 'haha@nghia.com', 'safsdfsadfasd'),
+(4, 'asdfasdf', '123456789', 'pdt@gmail.com', 'salkdfjalskdjfklsdfj'),
+(5, 'afasdfasdf', 'afsdfasdfasd', 'pdt@gmail.com', 'asdfasdf'),
+(6, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'salkdfjalskdjfklsdfj'),
+(7, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'salkdfjalskdjfklsdfj'),
+(8, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'salkdfjalskdjfklsdfj'),
+(9, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'asdfasdf'),
+(10, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'asdfasdf'),
+(11, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'asdfasdf'),
+(12, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'salkdfjalskdjfklsdfj'),
+(13, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'salkdfjalskdjfklsdfj'),
+(14, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'salkdfjalskdjfklsdfj'),
+(15, 'Trần Thanh Phương', '6132165165121', 'pdt@gmail.com', 'salkdfjalskdjfklsdfj'),
+(16, 'asdfasdf', '6132165165121', 'pdt@gmail.com', 'asdfasdf');
 
 -- --------------------------------------------------------
 
@@ -163,13 +213,47 @@ INSERT INTO `san_pham` (`id_sp`, `ten_sp`, `id_loai`, `don_gia`, `hinh`) VALUES
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `role_user` tinyint(3) NOT NULL,
-  `email_user` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  `password_user` int(11) NOT NULL,
-  `ten_user` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='User';
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(300) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `ho_ten` varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `so_dien_thoai` varchar(50) NOT NULL,
+  `dia_chi` varchar(300) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `role` tinyint(3) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1 COMMENT='User';
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `ho_ten`, `so_dien_thoai`, `dia_chi`, `role`) VALUES
+(12, 'test2@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'TEST lần 2', '000909099', 'test 2', 0),
+(16, 'phuong@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Thanh PHương', '074027272', 'TEST địa chỉ', 0),
+(18, 'kiet@gmail.com', 'fcea920f7412b5da7be0cf42b8c93759', 'Kiệt LÂm', '000009090', 'test địa chỉ Kiệt', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voucher`
+--
+
+DROP TABLE IF EXISTS `voucher`;
+CREATE TABLE IF NOT EXISTS `voucher` (
+  `id_voucher` int(11) NOT NULL AUTO_INCREMENT,
+  `voucher_code` char(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `voucher_value` int(3) NOT NULL,
+  PRIMARY KEY (`id_voucher`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='voucher';
+
+--
+-- Dumping data for table `voucher`
+--
+
+INSERT INTO `voucher` (`id_voucher`, `voucher_code`, `voucher_value`) VALUES
+(1, 'MISSMESSI', 10),
+(2, '10DIEM', 500);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
